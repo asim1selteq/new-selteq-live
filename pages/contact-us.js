375befa7af0d5028a6d7ec70005d32c103ba6fe8
asim1selteq <asim@selteq.net>
2023-06-06 20:32:32 +0500

    *

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -22,6 +22,7 @@ const ContactUs = () => {
         <title>Got A Question? Our Experts Are Ready to Help | Selteq</title>
         <meta name="description" content="Discover the best-of-breed software solutions for your business. Whatever your business challenge, Selteq is here to help." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        
         {/* Favicon icon  */}
         <link rel="icon" href="/favicon.ico" />
         {/* bootstrap v4.0.0 cdn  */}
@@ -38,6 +39,7 @@ const ContactUs = () => {
           integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
           crossorigin="anonymous"
         />
+        
       </Head>
       <main className={styles.main_head}>
         <div className='col-md-12 px-0 display_h'>
