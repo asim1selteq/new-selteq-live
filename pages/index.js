0609b6a0569931ffe453635d32ee1345de7176b8
asim1selteq <asim@selteq.net>
2023-06-06 16:15:01 +0500

    *

--- pages/index.js
+++ pages/index.js
@@ -73,7 +73,7 @@ export default function Home({ posts }) {
         <Newheader />
         <Video />
         <Youdream />
-        <ServicesSlider/>
+        {/* <ServicesSlider/> */}
         <Webdevsec />
         <Parallex />
         <Videosec />
